6426df0dc5e8fc317fdb4fe6e4bdf2d5b8884f1c
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2018-03-28 19:04:14 +0300

    add authentication etc

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import consign from "consign";
 
 const app = express();
 
-app.set("json spaces", 4);
-
-consign()
-    .include("db.js")
-    .then("models")
+consign({verbose: false})
+    .include("libs/config.js")
+    .then("db.js")
+    .then("auth.js")
     .then("libs/middlewares.js")
     .then("routes")
     .then("libs/boot.js")
     .into(app);
 
-app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
+module.exports = app;
